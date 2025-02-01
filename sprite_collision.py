406fcce5b6745fcb80d499d50844291af2f474bb
ajmainwasif01 <nipunazmun@gmail.com>
2025-02-01 17:19:51 +0600

    Sprite Collision

--- sprite_collision.py
+++ sprite_collision.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 500,400
+SCREEN_WIDTH, SCREEN_HEIGHT = 400,400
 MOVEMENT_SPEED = 5
 FONT_SIZE = 72
 
@@ -10,14 +10,14 @@ pygame.init()
 background_img = pygame.transform.scale(pygame.image.load("bg15.jpg"),
                                         (SCREEN_HEIGHT,SCREEN_WIDTH))
 
-font = pygame.font.Sysfont("Times New Roman", FONT_SIZE)
+font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
 
 
 class Sprite(pygame.sprite.Sprite):
 
     def __init__(self,color,height,width):
         super().__init__()
-        self.image = pygame.Surface(width,height)
+        self.image = pygame.Surface([width,height])
         self.image.fill(pygame.Color("dodgerblue"))
         pygame.draw.rect(self.image,color,pygame.Rect(0,0, width, height))
         self.rect = self.image.get_rect()
@@ -32,9 +32,44 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Sprite Collision")
 all_sprites =pygame.sprite.Group()
 
-sprite1 = Sprite(pygame.Color("darkblue"),20,30)
+sprite1 = Sprite(pygame.Color('darkblue'),20,30)
 sprite1.rect.x, sprite1.rect.y = random.randint(0,SCREEN_WIDTH - sprite1.rect.width),random.randint(0 ,SCREEN_HEIGHT - sprite1.rect.height)
 all_sprites.add(sprite1)
 
 sprite2 = Sprite(pygame.Color("red"),20,30)
 sprite2.rect.x, sprite2.rect.y = random.randint(0,SCREEN_WIDTH - sprite2.rect.width), random.randint(0,SCREEN_HEIGHT - sprite2.rect.height)
+all_sprites.add(sprite2)
+
+running, won = True, False
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN
+                                         and event.key == pygame.K_x):
+         running = False
+
+    if not won:
+        keys = pygame.key.get_pressed()
+        x_change = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * MOVEMENT_SPEED
+        y_change = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * MOVEMENT_SPEED
+        sprite1.move(x_change, y_change)
+
+
+        if sprite1.rect.colliderect(sprite2):
+            all_sprites.remove(sprite2)
+            won = True
+
+
+    screen.blit(background_img, (0,0))
+    all_sprites.draw(screen)
+
+    if won:
+        win_text = font.render("You Win!", True,pygame.Color('black'))
+        screen.blit(win_text , ((SCREEN_WIDTH - win_text.get_width())// 2,
+                                (SCREEN_HEIGHT - win_text.get_height()) // 2))
+
+    pygame.display.flip()
+    clock.tick(90)
+
+pygame.quit()
